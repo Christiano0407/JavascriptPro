a83b4e23036b2fbc72c5e40c731be44f3c0aa67d
Christiano0407 <placebo0704@gmail.com>
2020-01-06 11:26:46 -0600

    play

--- javascript/javas.js
+++ javascript/javas.js
@@ -1,1 +1,17 @@
-console.log(`Hola!`);
+const video = document.querySelector(`video`);
+const button = document.querySelector(`button`);
+
+function MediaPlayer(config) {
+    this.media = config.el;
+}
+
+MediaPlayer.prototype.toggleplay = function(){
+    if(this.media.paused){
+        this.media.play()
+    }else{
+        this.media.pause()
+    }
+}
+
+const player = new MediaPlayer({el: video});
+button.onclick = () => player.toggleplay();
