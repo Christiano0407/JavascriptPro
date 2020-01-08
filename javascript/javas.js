66886cc76899b0186ef29ab3f4ab71ad566928c1
Christiano0407 <placebo0704@gmail.com>
2020-01-08 08:27:58 -0600

    this

--- javascript/javas.js
+++ javascript/javas.js
@@ -5,6 +5,14 @@ function MediaPlayer(config) {
     this.media = config.el;
 }
 
+MediaPlayer.prototype.play = function () {
+    this.media.play()
+}
+
+MediaPlayer.prototype.pause = function() {
+    this.media.pause()
+}
+
 MediaPlayer.prototype.toggleplay = function(){
     if(this.media.paused){
         this.media.play()
