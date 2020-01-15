7eadebf687a409aa1c436017568118ebf140aa0d
Christiano0407 <placebo0704@gmail.com>
2020-01-15 08:39:50 -0600

    sombra

--- javascript/plugins/pause.js
+++ javascript/plugins/pause.js
@@ -1,16 +1,19 @@
 class pause {
+    constructor() {
+        this.threshold = 0.25
+        this.handlerIntersection = this.handlerIntersection.bind(this)
+    } 
+    
     run(player){ 
-        constructor() {
-            this.threshold = 0.25
-        }
-
+         
         this.player = player;
        const observer = new IntersectionObserver(this.handlerIntersection, {
      threshold: this.threshold
        }) 
 
-       observer.observe(player.media)
+       observer.observe(this.player.media)
     }
+
     handlerIntersection(entries) {
        const entry = entries[0];
 
