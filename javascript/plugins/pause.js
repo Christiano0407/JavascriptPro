49210b38d30d1166765bd6b4cd8eeb230306fd83
Christiano0407 <placebo0704@gmail.com>
2020-01-15 08:14:42 -0600

    play

--- javascript/plugins/pause.js
+++ javascript/plugins/pause.js
@@ -1,5 +1,29 @@
 class pause {
-    run(player){}
+    run(player){ 
+        constructor() {
+            this.threshold = 0.25
+        }
+
+        this.player = player;
+       const observer = new IntersectionObserver(this.handlerIntersection, {
+     threshold: this.threshold
+       }) 
+
+       observer.observe(player.media)
+    }
+    handlerIntersection(entries) {
+       const entry = entries[0];
+
+      const isVisible =  entry.intersectionRatio > this.threshold
+
+      if (isVisible){
+          this.player.play();
+
+      }else {
+          this.player.pause();
+
+      }
+    }
 }
 
 export default pause;
